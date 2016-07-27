1de5a682bc52a3fdcd0ce14d37e4e0c62a098b48
Zhou Hao <zhouhao@wowtv.com>
2016-07-27 16:49:57 +0800

    Publish to npm

---
+++ README.md
@@ -0,0 +1,3 @@
+This is for testing purpose only. Please don't use it in a production environment yet.
+
+The first version is only support Gmail.
---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var nodemailer = require('nodemailer');
+
+var SimpleNodemailerAdapter = nodemailerOptions => {
+  if (!nodemailerOptions || !nodemailerOptions.user || !nodemailerOptions.password || !nodemailerOptions.fromAddress) {
+    throw 'SimpleNodemailerAdapter requires an user name, password, and fromAddress.';
+  }
+
+  var sendMail = mail => {
+
+    var transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+          user: nodemailerOptions.user,
+          pass: nodemailerOptions.password
+      },
+      logger: true, // log to console
+      debug: true // include SMTP traffic in the logs
+    }, {
+        from: 'Sender Name <' + nodemailerOptions.fromAddress + '>',
+    });
+
+    var message = {
+      to: '"Receiver Name" <' + mail.to + '>',
+      subject: mail.subject,      
+      text: mail.text
+    };
+
+    return new Promise((resolve, reject) => {
+      transporter.sendMail(message, (error, info) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(info);
+      });
+    });
+  }
+
+  return Object.freeze({
+    sendMail: sendMail
+  });
+}
+
+module.exports = SimpleNodemailerAdapter
